8aad4b7bb71af856fd27f4b3117e9f765e29733b
Alex <nguyen.alexandra1@gmail.com>
2020-01-29 14:51:10 +0100

    display of users and mimos

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql } from 'apollo-boost';
+import { useQuery} from '@apollo/react-hooks';
 
-function App() {
+const GET_ALL_USERS_AND_MIMOS = gql`
+  query {
+    allUsers {
+      id
+      username
+    }
+    getMimos {
+      id
+      title
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_ALL_USERS_AND_MIMOS);
+  if (loading) return <p>Loading...</p>;
+  if (error) console.error(error);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Users:</h1>
+      {data.allUsers && 
+        data.allUsers.map(user => <li key={user.id}>{user.username} (id {user.id})</li>)
+      }
+      <h1>Mimos:</h1>
+      {data.getMimos && 
+        data.getMimos.map(mimo => <li key={mimo.id}>{mimo.title} (id {mimo.id})</li>)
+      }
+    </>
+  )
 }
+
+
 
 export default App;
